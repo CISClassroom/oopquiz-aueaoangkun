8bed702e82a2270b0c9d37b06b5e8e7c20ad46f2
Fin <aueaoangkun@kkumail.com>
2020-03-13 15:54:48 +0700

    Q

---
+++ mavenproject8/src/main/java/com/mycompany/mavenproject8/cal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject8;
+
+/**
+ *
+ * @author Lenovo Lab
+ */
+public class cal {
+    public double Lab(double number1, double number2 , double number3 , double number4 , double number5 , double result){
+        return (number1 + number2 + number3 + number4 + number5)/result;
+    }
+}
